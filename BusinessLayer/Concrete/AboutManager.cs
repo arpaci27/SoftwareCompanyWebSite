bad3d92e7c0cc1df1906a91a97388db82feca56f
Ömer Arpacı <91760786+arpaci27@users.noreply.github.com>
2024-02-11 19:12:18 +0300

    Traversal theme added

--- BusinessLayer/Concrete/AboutManager.cs
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -11,16 +11,21 @@ namespace BusinessLayer.Concrete
 {
     public class AboutManager : IAboutService
     {
-        IAboutDal aboutDal;
+        IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
 
         public void TAdd(About t)
         {
-            throw new NotImplementedException();
+            _aboutDal.Insert(t);
         }
 
         public void TDelete(About t)
         {
-            throw new NotImplementedException();
+           _aboutDal.Delete(t);
         }
 
         public About TGetByID(int id)
@@ -30,12 +35,12 @@ namespace BusinessLayer.Concrete
 
         public List<About> TGetList()
         {
-            throw new NotImplementedException();
+            return _aboutDal.GetList();
         }
 
         public void TUpdate(About t)
         {
-            throw new NotImplementedException();
+            _aboutDal.Update(t);
         }
     }
 }
