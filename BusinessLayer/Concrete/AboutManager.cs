6b398576f385f4ca718fb1c90e4e613373cc1079
Ömer Arpacı <91760786+arpaci27@users.noreply.github.com>
2024-02-11 18:37:03 +0300

    GenericServices

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Abstract;
+using DataAccesLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal aboutDal;
+
+        public void TAdd(About t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(About t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public About TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(About t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
