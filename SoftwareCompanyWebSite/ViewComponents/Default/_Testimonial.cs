c80ed23b950cf99fe575f092f3754967c314da6d
Ömer Arpacı <91760786+arpaci27@users.noreply.github.com>
2024-02-15 23:50:58 +0300

    Conroller added for Services of company

--- SoftwareCompanyWebSite/ViewComponents/Default/_Testimonial.cs
+++ SoftwareCompanyWebSite/ViewComponents/Default/_Testimonial.cs
@@ -1,6 +1,16 @@
-﻿namespace SoftwareCompanyWebSite.ViewComponents.Default
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SoftwareCompanyWebSite.ViewComponents.Default
 {
-    public class _Testimonial
+    public class _Testimonial: ViewComponent
     {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = testimonialManager.TGetList();
+            return View(values);
+        }
     }
 }
