72f9c51e406483f301d9a5fa96471feff260b0d8
Ömer Arpacı <91760786+arpaci27@users.noreply.github.com>
2024-02-16 00:38:31 +0300

    Services Page is modernized

--- SoftwareCompanyWebSite/Controllers/ServiceController.cs
+++ SoftwareCompanyWebSite/Controllers/ServiceController.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SoftwareCompanyWebSite.Controllers
 {
     public class ServiceController : Controller
     {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+
         public IActionResult Index()
         {
-            return View();
+            var values = serviceManager.TGetList();
+            return View(values);
         }
     }
 }
