8b2e760ec9b339171e1ea5ba3afd22cee6c0b39a
Ömer Arpacı <91760786+arpaci27@users.noreply.github.com>
2024-03-09 23:55:17 +0300

    Program.cs has configured instead of Startup.cs but its not working?

--- SoftwareCompanyWebSite/Program.cs
+++ SoftwareCompanyWebSite/Program.cs
@@ -1,7 +1,26 @@
+using DataAccesLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddMvc(config =>
+{
+    var policy = new AuthorizationPolicyBuilder().
+    RequireAuthenticatedUser().
+    Build();
+    config.Filters.Add(new AuthorizeFilter(policy));
+});
+
+builder.Services.AddMvc();
 
 var app = builder.Build();
 
@@ -15,7 +34,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseAuthentication();
 app.UseRouting();
 
 app.UseAuthorization();
